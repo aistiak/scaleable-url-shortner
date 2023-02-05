d6d95c0cdc16b1f6f4f06248a43decd53522b67c
aistiak <aristiak1994@gmail.com>
2023-02-05 19:29:11 +0600

    zk promisified

--- server/libs/getServerRange.js
+++ server/libs/getServerRange.js
@@ -4,53 +4,62 @@ const crypto = require("crypto")
 const getServerRange = async (zkClient, path) => {
 
 
-    // get root config 
+    return new Promise((resolve, reject) => {
+        // get root config 
 
-    // get all empheal nodes 
+        // get all empheal nodes 
 
-    const available = []
+        const available = []
 
-    console.log(`getting data `)
-    zkClient.getData(
-        path,
-        function (error, rawData, stat) {
-            console.log(`inside data `)
+        console.log(`getting data `)
+        zkClient.getData(
+            path,
+            function (error, rawData, stat) {
+                console.log(`inside data `)
 
-            if (error) { throw 'error getting range' }
-
-            const data = JSON.parse(rawData.toString())
-            console.log(data)
-            zkClient.getChildren(path, function (err, children, stats) {
-                if (err) {
-                    console.log(err.stack)
-                    return
+                if (error) {
+                    return resolve({ success: false, errors: ['error getting range'] })
                 }
-                console.log('children are' + children)
-                console.dir(children)
-                console.log(data.ranges)
-                data.ranges.map((v) => {
-                    if (!children.includes(v.id)) {
-                        available.push(v)
-                    }
-                })
-                console.dir({ available })
 
-                if (available.length) {
-                    const t = available[0]
-                    zkClient.create(`${path}/${t.id}`, Buffer.from('data'), zk.CreateMode.EPHEMERAL, function (err, path) {
-                        if (err) {
-                            console.log(err)
-                            return
+                const data = JSON.parse(rawData.toString())
+                console.log(data)
+                zkClient.getChildren(path, function (err, children, stats) {
+                    if (err) {
+                        console.log(err.stack)
+                        return resolve({ success: false })
+                    }
+                    console.log('children are' + children)
+                    console.dir(children)
+                    console.log(data.ranges)
+                    data.ranges.map((v) => {
+                        if (!children.includes(v.id)) {
+                            available.push(v)
                         }
                     })
-                }else {
-                    console.log(` --- no more ranges are available ---`)
-                }
+                    console.dir({ available })
 
-            })
-        }
-    )
-    console.log(`got data `)
+                    if (available.length) {
+                        const t = available[0]
+                        zkClient.create(`${path}/${t.id}`, Buffer.from('data'), zk.CreateMode.EPHEMERAL, function (err, path) {
+                            if (err) {
+                                console.log(err)
+                                return resolve({success : false})
+                            }
+                            return resolve({ success: true })
+                        })
+                    } else {
+                        console.log(` --- no more ranges are available ---`)
+                        return resolve({ success: false })
+                    }
+
+                })
+            }
+        )
+        console.log(`got data `)
+
+
+    })
+
 
 
 
@@ -65,5 +74,7 @@ module.exports = {
 
     await setUpZookeeper()
 
-    await getServerRange(zkClient, '/poc')
+    const r1 = await getServerRange(zkClient, '/poc')
+
+    console.log({ r1: r1 })
 })()
