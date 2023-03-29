0a3572e798ce6ebbcd567aa093fba39d8057302f
aistiak <aristiak1994@gmail.com>
2023-03-29 16:23:06 +0600

    clipboard and tooltip added

--- frontend/src/components/table.tsx
+++ frontend/src/components/table.tsx
@@ -1,19 +1,42 @@
 import DashboardLayout from "@/layouts/dashboard";
 import dynamic from "next/dynamic";
 import { useCallback, useMemo, useState } from "react";
-
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
+import { BiCopy } from 'react-icons/bi';
+import { toast } from "react-toastify";
 const DataTable = dynamic(() => import('react-data-table-component'), {
     ssr: false,
-  });
+});
 
 const columns = [
     {
         name: 'shortened',
-        selector: (row : any) => row.hash as string ,
+        selector: (row: any) => <div style={{display:'flex',alignItems:'center',cursor:'pointer'}} >
+            <div>
+                {row.hash}
+            </div>
+            <div style={{marginLeft:'5px',fontSize:'20px'}} onClick={()=>{
+                navigator.clipboard.writeText(row.hash)
+                toast.success('copied to clipboard')
+            }} >
+                <BiCopy />
+            </div>
+        </div>
     },
     {
         name: 'original',
-        selector: (row : any) => `${row.url.slice(0,50)}...` as string ,
+        selector: (row: any) => <div>
+            <span id={`anchor-id-${row.id}`}>
+                {`${row.url.slice(0, 50)}...`}
+            </span>
+            <ReactTooltip
+                data-tooltip-id={`anchor-id-${row.id}`}
+                anchorId={`anchor-id-${row.id}`}
+                place="bottom"
+                content={row.url}
+            />
+        </div>,
     },
 ];
 
@@ -29,16 +52,16 @@ const tableDataItems = [
         url: 'https://stackoverflow.com/questions/59475858/next-js-single-page-application',
     }
 ]
-function Table () {
+function Table() {
     const [selectedRows, setSelectedRows] = useState([]);
     const [toggleCleared, setToggleCleared] = useState(false);
     const [data, setData] = useState(tableDataItems);
 
-    const handleRowSelected = useCallback((state : any) => {
+    const handleRowSelected = useCallback((state: any) => {
         setSelectedRows(state.selectedRows);
     }, []);
     // @ts-ignore
-    const Button = ({children,etc}) => {
+    const Button = ({ children, etc }) => {
         return (
             <button {...etc}>{children}</button>
         )
@@ -61,24 +84,24 @@ function Table () {
     }, [data, selectedRows, toggleCleared])
     return (
         <DataTable
-        title="Shortened URLs"
-        columns={columns}
-        data={data}
-        
-        // paginationPerPage={10}
-        // paginationTotalRows={123}
-        onChangePage={(args)=>{
-            console.log(args)
-            console.log(` --- on change page ---`)
-        }}
-        selectableRows
-        contextActions={contextActions}
-        onSelectedRowsChange={handleRowSelected}
-        clearSelectedRows={toggleCleared}
-        pagination
-        paginationServer={false}
-    />
+            title="Shortened URLs"
+            columns={columns}
+            data={data}
+
+            // paginationPerPage={10}
+            // paginationTotalRows={123}
+            onChangePage={(args) => {
+                console.log(args)
+                console.log(` --- on change page ---`)
+            }}
+            selectableRows
+            contextActions={contextActions}
+            onSelectedRowsChange={handleRowSelected}
+            clearSelectedRows={toggleCleared}
+            pagination
+            paginationServer={false}
+        />
     )
 }
 
-export default Table ;
+export default Table;
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,14 +1,16 @@
 
 import Table from "@/components/table";
 import DashboardLayout from "@/layouts/dashboard";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function Dashboard() {
 
     return (
         <DashboardLayout>
-            <div>
-                <Table/>
-            </div>
+
+            <Table />
+            <ToastContainer />
         </DashboardLayout>
     )
 }
