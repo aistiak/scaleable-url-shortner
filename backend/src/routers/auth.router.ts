28cf7eec6de5a502b5e93ed3d389daa56c96130b
Arif Istiak <aristiak1994@gmail.com>
2023-08-22 18:49:53 +0600

    update

--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import AuthController from "../controllers/auth.controller";
 
 
-
+  
 const AuthRouter = Router() ;
 
 const authController = new AuthController()
